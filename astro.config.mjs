cdd9b7ea5989168c24758c7d0349b74a2a33f6d3
zhongyangxun <zhongyangxun@163.com>
2026-03-03 15:00:15 +0800

    chore: add react, tailwindcss and shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zhongyangxun.github.io',
   base: '/blog',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
 });
