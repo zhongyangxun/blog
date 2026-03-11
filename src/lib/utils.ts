5a715c6d1777b5ff1f76fc9eb52ef48c2feaebf3
zhongyangxun <zhongyangxun@gmail.com>
2026-03-11 15:19:49 +0800

    refactor: mv dateFormate function to src/lib/utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,3 +2,13 @@ import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+
+export const formatDate = (date: Date) => {
+  return date
+    .toLocaleDateString('zh-CN', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    })
+    .replace(/\//g, '.');
+};
