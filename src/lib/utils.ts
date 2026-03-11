193804bd99e8cc234efee60f8fe197402961297c
zhongyangxun <zhongyangxun@gmail.com>
2026-03-11 15:26:20 +0800

    style: refine date format visual (dots to slashes)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,11 +4,9 @@ import { twMerge } from 'tailwind-merge';
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
 export const formatDate = (date: Date) => {
-  return date
-    .toLocaleDateString('zh-CN', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-    })
-    .replace(/\//g, '.');
+  return date.toLocaleDateString('zh-CN', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
 };
